096f72cbf6fd85e0196bf01d691d77e77b6456cd
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2023-09-12 18:56:32 +0800

    Add the conversion and display it

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-const currencyDropdown = document.getElementById("currency");
-const submitButton = document.querySelector(".submit");
+let usd, myr, gbp;
 
-let selectedCurrency = ""
-currencyDropdown.addEventListener("change", function(){
-    selectedCurrency = currencyDropdown.value;
+document.querySelector("form").addEventListener("submit", function(eventPrint){
+   eventPrint.preventDefault();
 
+   const selectedCurrency = document.getElementById("currency").value;
+
+   const amountInput = document.getElementById("num");
+   const enteredAmount = parseFloat(amountInput.value);
+
+   
+
+   function calcCurrency(enteredAmount)
+   {
+      switch(selectedCurrency)
+      {
+        case "usd":
+          usd = enteredAmount;
+          myr = (enteredAmount * 4.68).toFixed(2);
+          gbp = (enteredAmount * 0.80).toFixed(2);
+          break;
+        
+        case "myr":
+          myr = enteredAmount;
+          usd = (enteredAmount * 0.21).toFixed(2);
+          gbp = (enteredAmount * 0.17).toFixed(2);
+          break;
+        
+        case "gbp":
+          gbp = enteredAmount;
+          usd = (enteredAmount * 1.25).toFixed(2);
+          myr = (enteredAmount *  5.83).toFixed(2);
+      }
+
+     
+   }
+   
+   const resultElement = document.getElementById("result");
+   calcCurrency(enteredAmount);
+   resultElement.innerHTML = "Selected Currency: " + gbp + "£ " + usd + "$ " + myr + "MYR";
+
+   
 });
 
-submitButton.addEventListener("click", function(){
-  const resultDiv = document.getElementById("result");
 
-  if(selectedCurrency){
-    
-    resultDiv.textContent = "Currency: " + selectedCurrency;
-  }
-  else{
-    resultDiv.textContent = "Please select a currency.";
-  }
-});
+  
+
+
 
